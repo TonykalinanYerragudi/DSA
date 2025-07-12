18a4084273583a672554eb9aa1f079b46c593873
Tony Kalinan <yerraguditonykalinan@gmail.com>
2025-07-12 23:26:42 +0530

    dynamic array

--- Topics/Arrays.cs
+++ Topics/Arrays.cs
@@ -7,19 +7,27 @@ insert in a postion. O(n)
 delete. O(n)
 search with index. O(1)
 
+static
 int[] arr = new int[5];
 int[] arr = {1, 2, 3, 4};
 
+dynamic array
+List<int> list = new List<int>();
 */
 using System;
+//static array
 int[] arr = new int[5];
 
+//dynamic arrar
+List<int> list = new List<int>();
 for(int i = 0; i < arr.Length; i++)
 {
   arr[i] = i + 1;
+  list.Add(i+1);
 }
 
 for (int i = 0; i < arr.Length; i++)
 {
-  Console.WriteLine(arr[i]);
+  Console.WriteLine("array: "+ arr[i]);
+  Console.WriteLine("list: "+ list[i]);
 }
