ee8e2a80aa4e3228a0f13ce0b8aff14e39ee3a41
Tony Kalinan <yerraguditonykalinan@gmail.com>
2025-07-13 22:48:22 +0530

    array implementation

--- Topics/Arrays.cs
+++ Topics/Arrays.cs
@@ -36,3 +36,67 @@ List<object> objects = new List<object>(){"string", 1, true};
 foreach(var obj in objects){
   Console.WriteLine(obj);
 }
+
+public class MyArray{
+  public int Length{get; set;}
+  private Dictionary<int, object> data;
+  
+  public MyArray(){
+    this.Length = 0;
+    this.data = new Dictionary<int, object>();
+  }
+
+  public object Get(int index){
+    return this.data[index];
+  }
+
+  public void Push(object obj){
+    this.data[this.Length] = obj;
+    this.Length++;
+  }
+
+  public object Pop(){
+    var obj = this.data[this.Length-1];
+    this.data.Remove(this.Length-1);
+    this.Length--;
+    return obj;
+  }
+
+  public void Delete(int index){
+    var item = this.data[index];
+    Shiftitems(index);
+  }
+
+ // delete index 1
+ // hi hello you now
+ // hi you now 
+  public void Shiftitems(int index){
+    for(int i=index; i<this.Length - 1; i++){
+      this.data[i] = this.data[i+1];
+    }
+    this.data.Remove(this.Length-1);
+    this.Length--;
+  }
+
+  public override string ToString()
+    {
+        if (this.Length == 0)
+            return "MyArray is empty.";
+
+        string result = "MyArray: ";
+        for (int i = 0; i < this.Length; i++)
+        {
+            result += this.data[i] + " ";
+        }
+        return result.Trim();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var myarr = new MyArray();
+        Console.WriteLine(myarr.ToString()); 
+    }
+}
