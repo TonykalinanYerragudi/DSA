74d534f6d5f3dcc554491f7f14aa8b0e6c2a8532
Tony Kalinan <yerraguditonykalinan@gmail.com>
2025-07-16 01:05:32 +0530

    moves zeroes optimal solution

--- Practice/283. Move Zeroes.cs
+++ Practice/283. Move Zeroes.cs
@@ -9,16 +9,29 @@ Input: nums = [0]
 Output: [0]
 
 */
-  //brute force O(n**2)
+// optimal solution: O(n)
   public void MoveZeroes(int[] nums) {
-        for(int i=0; i<nums.Length-1; i++){
-            for(int j=i+1; j<nums.Length; j++){
-                if(nums[i]==0){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
+        int l = 0;
+        for(int i=0; i<nums.Length; i++){
+            if(nums[i] != 0){
+                int temp = nums[l];
+                nums[l] = nums[i];
+                nums[i] = temp;
+                l++;
             }
         }
     }
+
+  // //brute force O(n**2)
+  // public void MoveZeroes(int[] nums) {
+  //       for(int i=0; i<nums.Length-1; i++){
+  //           for(int j=i+1; j<nums.Length; j++){
+  //               if(nums[i]==0){
+  //                   int temp = nums[i];
+  //                   nums[i] = nums[j];
+  //                   nums[j] = temp;
+  //               }
+  //           }
+  //       }
+  //   }
 
