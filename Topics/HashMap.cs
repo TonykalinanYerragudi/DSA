9abf286268c28558da792fc4abc90667d093498b
Tony Kalinan <yerraguditonykalinan@gmail.com>
2025-07-18 00:50:18 +0530

    Dictionaries

---
+++ Topics/HashMap.cs
@@ -0,0 +1,13 @@
+/*Dictionaries
+
+For eg:
+Key -> blackbox -> Memory address
+"apple" -> blackbox -> 843
+ --------------
+| Key  | Value |
+| 843  | 900   |
+ --------------
+
+A Hash function is a function that generates a value of fixed
+length for each input that it gets.
+*/
