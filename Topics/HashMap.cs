bba7fb1e57587cc8c6272a408f1fdd703b666b8c
Tony Kalinan <yerraguditonykalinan@gmail.com>
2025-07-19 00:24:33 +0530

    HashMap

--- Topics/HashMap.cs
+++ Topics/HashMap.cs
@@ -10,4 +10,11 @@ Key -> blackbox -> Memory address
 
 A Hash function is a function that generates a value of fixed
 length for each input that it gets.
+
+insert O(1)
+lookup O(1)
+delete O(1)
+search O(1)
+
+Hash Collisions
 */
