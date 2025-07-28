da3cf103c6f3bd98d37bd28226a798d7cbf2c7db
Tony Kalinan <yerraguditonykalinan@gmail.com>
2025-07-29 00:34:04 +0530

    first recurring character

--- Topics/HashMap.cs
+++ Topics/HashMap.cs
@@ -24,3 +24,26 @@ Hash Collisions
 4. ConcurrentDictionary<TKey, TValue>
 5. ImmutableDictionary<TKey, TValue>
 */
+
+List<int> list = new List<int>()
+{
+  //2, 5, 1, 2, 3, 4, 6, 3, 2
+  //2, 1, 1, 3, 343, 2, 43
+  2, 3, 4, 6
+};
+var map = new Dictionary<int, int>();
+int map_map()
+{
+  for (int i = 0; i < list.Count; i++)
+  {
+    int n = list[i];
+    if (!map.ContainsKey(n))
+    {
+      map[n] = 1;
+      continue;
+    }
+    return n;
+  }
+  return 0;
+}
+Console.WriteLine(map_map());
