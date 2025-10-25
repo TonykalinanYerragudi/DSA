5fdf4399cbc8bed534da3c619f366d5c4cb6724b
Tony Kalinan <yerraguditonykalinan@gmail.com>
2025-10-26 00:26:40 +0530

    recursionlearning

---
+++ datastructuresandalgorithms/Recursion.cs
@@ -0,0 +1,48 @@
+/*
+"When a function calls itself" until a specific condition is met
+
+f(){
+print()
+f()    -- calling a function itself 
+}
+
+main(){
+f()
+}
+
+Now in above example there is no specific condition, so it will leads to stackOverFlow.
+        .....
+|   ....|   .  
+| f()   |   .
+| f()   |
+ _______
+
+with base condition:
+*/
+using System;
+
+int count = 0;
+
+int fun()
+{
+  if (count == 5)
+  {
+    return count;
+  }
+  Console.WriteLine(count);
+  count++;
+  return fun();
+}
+
+fun();
+
+/*
+output:
+
+0
+1
+2
+3
+4
+
+*/
